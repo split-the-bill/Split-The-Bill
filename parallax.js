569075ff0dd6fc4b6ecfffa2af543530232bef60
jtwray <tuckerwray84@gmail.com>
2019-05-18 01:25:24 -0400

    Defines constructor function of ParallaxImage(imag2p); assigns data-img Indices to images and accompanying divcontents

--- parallax.js
+++ parallax.js
@@ -1,3 +1,17 @@
+class ParallaxImage {
+    constructor(image2p) {
+            this.image2p = image2p;
+            //article.datset.columns
+            //this.link.dataset.tab
+            this.image2pData = this.image2p.dataset.img;
+            this.divContent = document.querySelector(`.content-wrapper[data-img="${this.image2pData}"]`);
+            console.log(this.divContent)
+        }
+        //methods
+}
+
+
+
 // stepUno selects all images with class bcg 
 // and assigns them to a NodeListof<Elements> imgPx
 const imgsToParallax = document.querySelectorAll('.bcg')
